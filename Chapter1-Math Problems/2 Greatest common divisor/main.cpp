1d9983852bb35690293d9cc90f3077dd71da8122
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-07-14 19:42:37 -0300

    Chapter 1

--- Chapter1-Math Problems/2 Greatest common divisor/main.cpp
+++ Chapter1-Math Problems/2 Greatest common divisor/main.cpp
@@ -1,15 +1,44 @@
+/*
+/***************************************\
+#2 Greatest common divisor
+=========================================
+
+Escriba un programa que, dados dos enteros positivos,
+calcule e imprima el máximo común divisor de los dos.
+
+*/
+
+/*
+/***************************************\
+Solutions:
+=========================================
+El máximo común divisor (mcd en resumen) de dos o más números enteros distintos de cero,
+también conocido como greatest common factor (gcf), highest common factor (hcf), 
+greatest common measure (gcm) o máximo común divisor, es el mayor entero positivo 
+que los divide a todos.
+Hay varias formas de calcular el gcd; un método eficiente es el algoritmo de Euclides.
+Para dos enteros, el algoritmo es:
+*/
+
 #include <iostream>
+#include <conio.h>
+
+using namespace std;
 
 int main() {
+
     int a, b;
-    std::cout << "Enter two numbers: ";
-    std::cin >> a >> b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
     int gcd = 1;
     for (int i = 1; i <= a && i <= b; i++) {
         if (a % i == 0 && b % i == 0) {
             gcd = i;
         }
     }
-    std::cout << "GCD of " << a << " and " << b << " is " << gcd << std::endl;
+    cout << "GCD of " << a << " and " << b << " is " << gcd << endl; 
+
+    getch();
     return 0;
-}
+};
